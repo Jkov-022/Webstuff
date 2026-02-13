54a6a6dbaf0bc3294a2bd81f03a17dcd8cfba373
Jkov-022 <neiliktoncaneda0218jack@gmail.com>
2026-02-13 18:08:38 +0800

    Fourth Commit

---
+++ guat/navbar.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="css/navbarstyle.css">
+<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+
+<nav>
+    <div class="leftContainer">
+        <p>Sylvia's</p>
+    </div>
+    <div></div>
+</nav>
